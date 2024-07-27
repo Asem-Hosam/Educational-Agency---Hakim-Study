3859f885876118ed2fcdc34af778840e885c1184
Asem-Hosam <asem8eladl@gmail.com>
2024-07-27 14:11:29 +0300

    scroll logic finished

--- components/explorebar/explorebar.js
+++ components/explorebar/explorebar.js
@@ -1,16 +1,46 @@
+'use client'
 import Excard from "./excard";
 import "./explorebar.css"
+import { useState, useRef } from "react";
 import { BuildingLibraryIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 
 export default function Explorebar() {
+
+    const [isScrolled, setIsScrolled] = useState(0);
+    const scrollContainerRef = useRef(null);
+
+
+    const scrollHandler = (event) => {
+        const scrollLeft = event.target.scrollLeft;
+        setIsScrolled(scrollLeft > 0);
+    };
+
+    function scrollLeft() {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollBy({
+                left: -100,
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    function scrollRight() {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollBy({
+                left: 100,
+                behavior: 'smooth',
+            });
+        }
+    };
+
     return (
         <div className={"container"}>
-            <div className={"btn1"}>
-                <button><ChevronLeftIcon /></button>
+            <div className={`btn1 ${isScrolled ? 'scrolled' : ''}`}>
+                <button onClick={scrollLeft} ><ChevronLeftIcon /></button>
             </div>
-            <div className={"overlay"}>
+            <div className={`overlay ${isScrolled ? 'scrolled' : ''}`}>
                 <div
-                    className={"hodlder"}>
+                    className={"hodlder"} onScroll={scrollHandler} ref={scrollContainerRef}>
                     <Excard href={"/"} title={"Universities"} icon={<BuildingLibraryIcon />} />
                     <Excard href={"/"} title={"University"} icon={<BuildingLibraryIcon />} />
                     <Excard href={"/"} title={"University"} icon={<BuildingLibraryIcon />} />
@@ -29,7 +59,7 @@ export default function Explorebar() {
                 </div>
             </div>
             <div className={"btn2"}>
-                <button><ChevronRightIcon /></button>
+                <button onClick={scrollRight}><ChevronRightIcon /></button>
             </div>
         </div>
     );

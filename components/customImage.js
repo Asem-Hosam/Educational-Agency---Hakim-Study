352a5bf7bb1f4b0d81c7587cb5251a2e4b5622fc
Asem-Hosam <asem8eladl@gmail.com>
2024-08-02 17:19:19 +0300

    little-up

--- components/customImage.js
+++ components/customImage.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function CustomImage({ src, width, height, alt, className, sizes, blurDataURL, placeholder, srcset }) {
+export default function CustomImage({ src, width = 500, height = 500, alt = "image", className, sizes, blurDataURL, placeholder, srcset }) {
     return (
         <Image src={src} alt={alt} width={width} height={height} className={className} srcSet={srcset}
             sizes={sizes} priority placeholder={placeholder} blurDataURL={blurDataURL} />

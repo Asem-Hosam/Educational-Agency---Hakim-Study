c68467c79204e9aa356ce0f5a64229afc478c5ae
Asem-Hosam <asem8eladl@gmail.com>
2024-08-05 00:19:49 +0300

    up-bottomNavbar

--- components/bottomNavbar/bottomNavabar.js
+++ components/bottomNavbar/bottomNavabar.js
@@ -1,12 +1,13 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from 'react';
 import styles from "./bottomNavbar.module.css";
 import { UserCircleIcon, MagnifyingGlassIcon, HomeIcon, PaperAirplaneIcon } from '@heroicons/react/24/outline';
 import CustomLink from '../customLink';
+import { usePathname } from 'next/navigation';
 
 function BottomNavbar() {
+    const path = usePathname();
     const [isHidden, setIsHidden] = useState(false);
-    const [active, setActive] = useState('/');
 
     useEffect(() => {
         let lastScrollTop = 0;
@@ -26,36 +27,40 @@ function BottomNavbar() {
         };
     }, []);
 
-    const handleActive = (path) => {
-        setActive(path);
-    }
+    const links = [
+        { href: "/", icon: <HomeIcon className={styles.icon} />, label: "Home" },
+        { href: "/explore/universities", icon: <MagnifyingGlassIcon className={styles.icon} />, label: "Explore" },
+        { href: "/application", icon: <PaperAirplaneIcon className={styles.icon} />, label: "Application" },
+        { href: "/profile", icon: <UserCircleIcon className={styles.icon} />, label: "Profile" },
+    ];
+
+    const isActive = (linkHref) => {
+        const pathWithoutLocale = path.replace(/^\/(en|ar)/, '');
+
+        console.log(pathWithoutLocale);
+        if (linkHref === '/') {
+            return pathWithoutLocale === '/' || pathWithoutLocale === '';
+        } else if (linkHref.startsWith('/explore')) {
+            return pathWithoutLocale.startsWith('/explore');
+        } else {
+            return pathWithoutLocale === linkHref;
+        }
+    };
 
     return (
         <div className={`${styles.bottomNavabarContainer} ${isHidden ? styles.hidden : ''}`}>
-            <CustomLink href="/" className={`${styles.explore} ${active === '/' ? styles.active : ''}`} onClick={() => handleActive('/')}>
-                <span>
-                    <HomeIcon className={styles.icon} />
-                    <p>Home</p>
-                </span>
-            </CustomLink>
-            <CustomLink href="/about" className={`${styles.user} ${active === '/about' ? styles.active : ''}`} onClick={() => handleActive('/about')}>
-                <span>
-                    <MagnifyingGlassIcon className={styles.icon} />
-                    <p>Explore</p>
-                </span>
-            </CustomLink>
-            <CustomLink href="/about" className={`${styles.user} ${active === '/about' ? styles.active : ''}`} onClick={() => handleActive('/about')}>
-                <span>
-                    <PaperAirplaneIcon className={styles.icon} />
-                    <p>Applicaiton</p>
-                </span>
-            </CustomLink>
-            <CustomLink href="/you" className={`${styles.profile} ${active === '/profile' ? styles.active : ''}`} onClick={() => handleActive('/you')}>
-                <span>
-                    <UserCircleIcon className={styles.icon} />
-                    <p>Profile</p>
-                </span>
-            </CustomLink>
+            {links.map((link) => (
+                <CustomLink
+                    key={link.href}
+                    href={link.href}
+                    className={`${styles.link} ${isActive(link.href) ? styles.active : ''}`}
+                >
+                    <span>
+                        {link.icon}
+                        <p>{link.label}</p>
+                    </span>
+                </CustomLink>
+            ))}
         </div>
     );
 }

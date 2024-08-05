222c0dd3b8e05ad2fc143702cab29790dcfb550e
Asem-Hosam <asem8eladl@gmail.com>
2024-08-05 03:20:05 +0300

    up

--- components/searchbar/searchbar.js
+++ components/searchbar/searchbar.js
@@ -1,9 +1,30 @@
 import styles from "./searchbar.module.css";
+import CustomImage from './../customImage';
+import { getImage } from "../../utils/getImages";
+import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/outline';
+import LocaleSwitcher from "../localswitcher/localSwitcher";
 
-export default function Searchbar() {
+export default async function Searchbar() {
+
+    const imageSrc = await getImage('logoIcon');
+
     return (
         <div className={styles.container}>
-
+            <div className={styles.logo}>
+                <CustomImage src={imageSrc} alt={"image"} className={styles.image} />
+            </div>
+            <div className={styles.searchbar} >
+                <div className={styles.bg}>
+                    <div className={styles.searchIcon}>
+                        <MagnifyingGlassIcon className={styles.Sicon} />
+                    </div>
+                    <p>Search</p>
+                </div>
+            </div>
+            <div className={styles.uselog}>
+                <UserCircleIcon className={styles.Usericon} />
+                <LocaleSwitcher className={styles.icon} />
+            </div>
         </div>
     );
 }

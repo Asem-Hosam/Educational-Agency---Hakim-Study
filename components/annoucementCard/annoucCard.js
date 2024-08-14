9314a5b8181d755fa7c30dc8a54331a6773a9744
Asem-Hosam <asem8eladl@gmail.com>
2024-08-14 04:40:19 +0300

    updating annoucCard & its css

--- components/annoucementCard/annoucCard.js
+++ components/annoucementCard/annoucCard.js
@@ -1,31 +1,33 @@
 import styles from "./annoucCard.module.css"
 import CustomImage from './../customImage';
+import CustomLink from "../customLink";
 
 
-export default function AnnoucCard() {
+export default function AnnoucCard({ title, date, desc, author, imageUrl, id }) {
     return (
-        <div className={styles.cardContainer}>
-            <div className={styles.img}>
-                <CustomImage src={"https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"} className={styles.ImageContent} />
+        <CustomLink href={`/explore/announcements/${id}`}>
+            <div className={styles.cardContainer}>
+                <div className={styles.img}>
+                    <CustomImage src={imageUrl} className={styles.ImageContent} />
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.date}>
+                        <p>{date}</p>
+                    </div>
+                    <div className={styles.title}>
+                        <h6>{title}</h6>
+                    </div>
+                    <div className={styles.desc}>
+                        <p>{desc}</p>
+                    </div>
+                </div>
+                <div className={styles.author}>
+                    <div className={styles.authorImage}>
+                        <CustomImage src={"https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"} className={styles.Image} />
+                    </div>
+                    <p>{author}</p>
+                </div>
             </div>
-            <div className={styles.content}>
-                <div className={styles.date}>
-                    <p>30 jan 2024</p>
-                </div>
-                <div className={styles.title}>
-                    <h6>hello</h6>
-                </div>
-                <div className={styles.desc}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim culpa cupiditate minus similique perspiciatis consequatur odio optio quisquam dolores excepturi facilis a totam ea aut perferendis, earum hic modi dicta?</p>
-                </div>
-            </div>
-            <div className={styles.author}>
-                <h6>by</h6>
-                <div className={styles.authorImage}>
-                    <CustomImage src={"https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"} className={styles.Image} />
-                </div>
-                <p>Firas Tahan</p>
-            </div>
-        </div >
+        </CustomLink>
     );
 }

d5e610c9c7b6f53b2845338fef06b8f43e17678b
Asem-Hosam <asem8eladl@gmail.com>
2024-11-10 22:00:33 +0300

    up

--- next.config.mjs
+++ next.config.mjs
@@ -4,21 +4,22 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'hakim-study-nextjs-demo-users-image.s3.eu-north-1.amazonaws.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname:
+          'hakim-study-nextjs-demo-users-image.s3.eu-north-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);

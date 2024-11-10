e953d3468d92bfa86de10ef1390e977560f2ee72
Asem-Hosam <asem8eladl@gmail.com>
2024-11-10 22:05:55 +0300

    counter fixed

--- components/counter.js
+++ components/counter.js
@@ -1,14 +1,13 @@
 'use client';
 import React, { useEffect, useRef } from 'react';
 
-function Counter({ target }) {
+function Counter({ target, className }) {
     const counterRef = useRef(null);
 
     useEffect(() => {
         const countUp = (element, target) => {
             let count = 0;
-            const increment = target / 200; // Adjust this value to control the speed
-
+            const increment = target / 200;
             const updateCount = () => {
                 count += increment;
                 if (count < target) {
@@ -26,13 +25,13 @@ function Counter({ target }) {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
                     countUp(entry.target, target);
-                    observer.unobserve(entry.target); // Stop observing after counting up
+                    observer.unobserve(entry.target);
                 }
             });
         };
 
         const observerOptions = {
-            threshold: 1.0, // Trigger when the entire element is visible
+            threshold: 1.0,
         };
 
         const observer = new IntersectionObserver(observerCallback, observerOptions);
@@ -45,7 +44,7 @@ function Counter({ target }) {
         };
     }, [target]);
 
-    return <div ref={counterRef} className="counter">0</div>;
+    return <div ref={counterRef} className={className} >0</div>;
 };
 
 export default Counter;
